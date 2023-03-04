ee06f2754758b1f9ab6ee1e5da0c6b630b40b9d3
Adam Wilczak <adam.wilczak11@gmail.com>
2023-03-04 19:47:28 +0100

    added funcion

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-let bcgChange = document.querySelector(".nav__flicker");
-let move = document.querySelector(".nav__flicker");
-let body = document.querySelector(".body");
-// let nameChange = document.querySelector(".js-nav__OnOff--Text");
+{
+  const hello = () => {
+    console.log("Witaj zacny dev! Niech Bogowie klawiatury ci sprzyjają");
+  };
+  hello();
 
-bcgChange.addEventListener("click", () => {
-  body.classList.toggle("darkTheme");
-  move.classList.toggle("darkTheme");
+  const toggleBackground = () => {
+    body.classList.toggle("darkTheme");
+    move.classList.toggle("darkTheme");
 
-  // nameChange.innerText = body.classList.contains("darkTheme") ? "on" : "off";
-});
+    // nameChange.innerText = body.classList.contains("darkTheme") ? "on" : "off";
+  };
+
+  const bcgChange = document.querySelector(".js-nav__flicker");
+  const move = document.querySelector(".js-nav__flicker");
+  const body = document.querySelector(".js-body");
+  // let nameChange = document.querySelector(".js-nav__OnOff--Text");
+
+  bcgChange.addEventListener("click", toggleBackground);
+}
